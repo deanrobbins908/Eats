5fdc641d164e227c352d361274646fa023d9da1c
Dean Robbins <dean.robbins@gmail.com>
2014-02-01 14:59:14 -0500

    Chapter 9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
  def show
   	@user = User.find(params[:id])
+    @meals = @user.meals(params[:id])
   end
 
    def new
@@ -11,6 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:success] = "Thank you for signing up!"
       redirect_to @user
     else
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140122051148) do
+ActiveRecord::Schema.define(version: 20140201191045) do
+
+  create_table "meals", force: true do |t|
+    t.string   "title"
+    t.string   "location"
+    t.integer  "price"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -19,8 +28,10 @@ ActiveRecord::Schema.define(version: 20140122051148) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
