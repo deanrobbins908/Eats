d83e40010155d3a94cfc0f7875f7c74338a389e6
Dean Robbins <dean.robbins@gmail.com>
2014-02-09 12:10:29 -0500

    Old Model Remove

--- app/models/meal.rb
+++
@@ -1,9 +0,0 @@
-class Meal < ActiveRecord::Base
-belongs_to :user
-default_scope -> { order('created_at DESC') }
-validates :user_id, presence: true
-validates :title, presence: true, length: { maximum: 50 }
-validates :location, presence: true, length: { maximum: 20 }
-validates :price, presence: true, length: { maximum: 20 }
-
-end
