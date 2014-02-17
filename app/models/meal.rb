1878450170846c181f1a3dd600753c6aeeb8f6b8
Dean Robbins <dean.robbins@gmail.com>
2014-02-17 16:36:19 -0500

    Bunch

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -2,7 +2,7 @@ class Meal < ActiveRecord::Base
 	belongs_to :user
 	default_scope -> { order('created_at DESC') }
 	validates :user_id, presence:true
-	validates :title, presence:true, length: { maximum: 120}
+	validates :title, presence:true, length: { maximum: 70}
 	validates :content, presence:true, length: { maximum: 500}
 	validates :price, presence:true
 	validates :mealtype, presence:true
